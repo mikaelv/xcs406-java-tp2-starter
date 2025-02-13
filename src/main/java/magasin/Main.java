5a49e76e2e8e9414a45ed0013fbd2b4e92f18ad7
Mikael Valot <mikael.valot@efrei.fr>
2025-02-13 22:59:34 +0100

    added template Main

---
+++ src/main/java/magasin/Main.java
@@ -0,0 +1,12 @@
+package magasin;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+       
+        scanner.close();
+    }
+}
